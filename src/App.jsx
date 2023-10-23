19429fa3f002f42d32325a4e639a5ab240cd37d1
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-23 10:39:14 -0700

    Some basic react-bootstrap styling to form and table

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col';
 
 function ExpenseForm() {
   return (
-    <form action="">  
-      <label htmlFor="type-select">Type: 
-        <select name="expenseTypes" id="expenseTypes">
+    <Form>
+      <Form.Group className="mb-">
+        <Form.Label>Type:</Form.Label>
+        <Form.Select>
           <option value="card">Card</option>
           <option value="cash">Cash</option>
-          <option value="cryptocoin">Cryptocoin</option>
-          <option value="other">Other</option>
-        </select>
-      </label>
-      <label htmlFor="description">Name:
-        <input type="text" id="description" name="description" placeholder="What did you spend on?"/>
-      </label>
-      <label htmlFor="date">Date: 
-        <input type="date" name="date" id="date" />
-      </label>
-      <label htmlFor="amount">Amount:
-        <input type="number" />
-      </label>
-      <input type="submit" value="Add a new expense" />
-    </form>
+          <option value="Cryptocoin">Cryptocoin</option>
+          <option value="Other">Other</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Email address</Form.Label>
+      </Form.Group>
+    </Form>
+    // <form action="">  
+    //   <label htmlFor="type-select">Type: 
+    //     <select name="expenseTypes" id="expenseTypes"> 
+    //       <option value="card">Card</option>
+    //       <option value="cash">Cash</option>
+    //       <option value="cryptocoin">Cryptocoin</option>
+    //       <option value="other">Other</option>
+    //     </select>
+    //   </label>
+    //   <label htmlFor="description">Name:
+    //     <input type="text" id="description" name="description" placeholder="What did you spend on?"/>
+    //   </label>
+    //   <label htmlFor="date">Date: 
+    //     <input type="date" name="date" id="date" />
+    //   </label>
+    //   <label htmlFor="amount">Amount:
+    //     <input type="number" />
+    //   </label>
+    //   <input type="submit" value="Add a new expense" />
+    // </form>
     
   )
 }
 
 function ExpenseTable() {
   return (
-    <table>
-      <thead>
+    <Table>
+      <thead className="table-secondary">
         <tr>
           <th>Type</th>
           <th>Name</th>
@@ -40,8 +58,15 @@ function ExpenseTable() {
           <th>Amount</th>
         </tr>
       </thead>
-      <tbody></tbody>
-    </table>
+      <tbody>
+        <tr>
+          <td>Cash</td>
+          <td>Car</td>
+          <td>October 23rd</td>
+          <td>$27,000</td>
+        </tr>
+      </tbody>
+    </Table>
   )
 }
 
@@ -50,18 +75,16 @@ export default function FilterableExpenseTable() {
 
   return (
     <>
-      <div className="filterableExpenseTable">
+      <Container>
         <h1>Simple Expense Manager</h1>
         <h2>Add New Item</h2>
-        <div className="expenseForm">
+        <Row>
           <ExpenseForm />
-        </div> 
-        <div className="expenseTable">
+        </Row>
+        <Row>
           <ExpenseTable />
-        </div>
-      </div>
-      
-
+        </Row>
+      </Container>
     </>
   )
 }
