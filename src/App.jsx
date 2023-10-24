ce21570fd854bab12c0c5efc05a555e7ec581b3f
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-24 16:04:48 -0700

    remove react-bootstrap, add basic bootstrap styling to form and table

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,95 @@
 import { useState } from 'react';
 import './App.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col';
+// import Col from 'react-bootstrap/Col';
 
 function ExpenseForm() {
   return (
-    <Form>
-      <Form.Group className="mb-">
-        <Form.Label>Type:</Form.Label>
-        <Form.Select>
-          <option value="card">Card</option>
-          <option value="cash">Cash</option>
-          <option value="Cryptocoin">Cryptocoin</option>
-          <option value="Other">Other</option>
-        </Form.Select>
-      </Form.Group>
-      <Form.Group className='mb-3' controlId='formBasicEmail'>
-        <Form.Label>Email address</Form.Label>
-      </Form.Group>
-    </Form>
-    // <form action="">  
-    //   <label htmlFor="type-select">Type: 
-    //     <select name="expenseTypes" id="expenseTypes"> 
+    // <Form>
+    //   <Form.Group className="mb-">
+    //     <Form.Label>Type:</Form.Label>
+    //     <Form.Select>
     //       <option value="card">Card</option>
     //       <option value="cash">Cash</option>
-    //       <option value="cryptocoin">Cryptocoin</option>
-    //       <option value="other">Other</option>
-    //     </select>
-    //   </label>
-    //   <label htmlFor="description">Name:
-    //     <input type="text" id="description" name="description" placeholder="What did you spend on?"/>
-    //   </label>
-    //   <label htmlFor="date">Date: 
-    //     <input type="date" name="date" id="date" />
-    //   </label>
-    //   <label htmlFor="amount">Amount:
-    //     <input type="number" />
-    //   </label>
-    //   <input type="submit" value="Add a new expense" />
-    // </form>
+    //       <option value="Cryptocoin">Cryptocoin</option>
+    //       <option value="Other">Other</option>
+    //     </Form.Select>
+    //   </Form.Group>
+    //   <Form.Group className='mb-3' controlId='formBasicEmail'>
+    //     <Form.Label>Email address</Form.Label>
+    //   </Form.Group>
+    // </Form>
+    <div class="container">
+      <form action="">  
+        <div class="form-group row">
+          <label htmlFor="type-select" class="col-sm-3 col-form-label">Type: </label>
+          <div class="col-sm-9">
+            <select name="expenseTypes" id="expenseTypes" class="form-control"> 
+              <option value="card">Card</option>
+              <option value="cash">Cash</option>
+              <option value="cryptocoin">Cryptocoin</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+        </div>
+        
+        <div class="form-group row">
+          <label htmlFor="description" class="col-sm-3 col-form-label">Name: </label>
+          <div class="col-sm-9">
+            <input type="text" id="description" name="description" placeholder="What did you spend on?" class="form-control"/>
+          </div>
+        </div>
+        
+        <div class="form-group row">
+          <label htmlFor="date" class="col-sm-3 col-form-label">Date: </label>
+          <div class="col-sm-9">
+            <input type="date" name="date" id="date"  class="form-control"/>
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label" htmlFor="amount">Amount: </label>
+          <div class="col-sm-9">
+            <input type="number"  class="form-control"/>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-sm-9 offset-sm-3">
+            <button type="submit" class="btn btn-primary">Add a new expense</button> 
+          </div>
+        </div>
+      </form>
+    </div>
     
   )
 }
 
 function ExpenseTable() {
   return (
-    <Table>
-      <thead className="table-secondary">
-        <tr>
-          <th>Type</th>
-          <th>Name</th>
-          <th>Date</th>
-          <th>Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Cash</td>
-          <td>Car</td>
-          <td>October 23rd</td>
-          <td>$27,000</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cash</td>
+            <td>Car</td>
+            <td>October 23rd</td>
+            <td>$27,000</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
